b874a42b80bd8ea48d4f89fa9ce1d45b9d9e1053
peacefulinlonely <32615146+peacefulinlonely@users.noreply.github.com>
2018-10-10 15:58:50 +0700

    Add files via upload

---
+++ assets/mape.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mape" tilewidth="64" tileheight="64" tilecount="168" columns="14">
+ <tileoffset x="0" y="16"/>
+ <image source="../../../project_folder/Pre_2018/GameFreedom/res/mape.png" width="900" height="800"/>
+ <terraintypes>
+  <terrain name="grass" tile="0"/>
+  <terrain name="lava" tile="56"/>
+  <terrain name="sand" tile="98"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,0,0,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="28" terrain="0,0,0,0"/>
+ <tile id="29" terrain="0,0,0,0"/>
+ <tile id="30" terrain="0,0,0,0"/>
+ <tile id="31" terrain="0,0,0,0"/>
+ <tile id="32" terrain="0,0,0,0"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,0,0,0"/>
+ <tile id="42" terrain="0,0,0,0" probability="0.5"/>
+ <tile id="43" terrain="0,0,0,0" probability="0.5"/>
+ <tile id="44" terrain="0,0,0,0" probability="0.5"/>
+ <tile id="45" terrain="0,0,0,0" probability="0.5"/>
+ <tile id="46" terrain="0,0,0,0"/>
+ <tile id="47" terrain="0,0,0,0"/>
+ <tile id="48" terrain="0,0,0,0"/>
+ <tile id="56" terrain="1,1,1,1"/>
+ <tile id="57" terrain="1,1,1,1"/>
+ <tile id="58" terrain="1,1,1,1"/>
+ <tile id="59" terrain="1,1,1,1"/>
+ <tile id="60" terrain="1,1,1,1"/>
+ <tile id="61" terrain="1,1,1,1"/>
+ <tile id="62" terrain="1,1,1,1"/>
+ <tile id="63" terrain="1,1,1,1"/>
+ <tile id="64" terrain="1,1,1,1"/>
+ <tile id="70" terrain="1,0,0,1"/>
+ <tile id="71" terrain="0,1,1,0"/>
+ <tile id="72" terrain="1,1,0,1"/>
+ <tile id="73" terrain="1,0,1,1"/>
+ <tile id="74" terrain="0,1,1,1"/>
+ <tile id="75" terrain="1,1,1,0"/>
+ <tile id="76" terrain="0,0,0,1"/>
+ <tile id="77" terrain="1,0,0,0"/>
+ <tile id="78" terrain="0,1,0,0"/>
+ <tile id="84" terrain="0,0,1,0"/>
+ <tile id="85" terrain="1,1,0,0"/>
+ <tile id="86" terrain="0,0,1,1"/>
+ <tile id="87" terrain="0,1,0,1"/>
+ <tile id="88" terrain="1,0,1,0"/>
+ <tile id="89" terrain="0,1,0,1"/>
+ <tile id="90" terrain="1,0,1,0"/>
+ <tile id="91" terrain="1,1,0,0"/>
+ <tile id="92" terrain="0,0,1,1"/>
+ <tile id="98" terrain="2,2,2,2"/>
+ <tile id="99" terrain="2,2,2,2"/>
+ <tile id="100" terrain="2,2,2,2"/>
+ <tile id="101" terrain="2,2,2,2"/>
+ <tile id="102" terrain="2,2,2,2"/>
+ <tile id="103" terrain="2,2,2,2"/>
+ <tile id="104" terrain="2,2,2,2"/>
+ <tile id="105" terrain="2,2,2,2"/>
+ <tile id="106" terrain="2,2,2,2"/>
+ <tile id="112" terrain="0,2,2,2"/>
+ <tile id="113" terrain="2,0,2,2"/>
+ <tile id="114" terrain="2,2,2,0"/>
+ <tile id="115" terrain="2,2,0,2"/>
+ <tile id="116" terrain="2,0,0,0"/>
+ <tile id="117" terrain="0,2,0,0"/>
+ <tile id="118" terrain="0,0,0,2"/>
+ <tile id="119" terrain="0,0,2,0"/>
+ <tile id="120" terrain="0,2,0,2"/>
+ <tile id="126" terrain="0,0,2,2"/>
+ <tile id="127" terrain="2,0,2,0"/>
+ <tile id="128" terrain="2,2,0,0"/>
+ <tile id="129" terrain="0,2,0,2"/>
+ <tile id="130" terrain="0,2,2,0"/>
+ <tile id="131" terrain="2,0,0,2"/>
+ <tile id="132" terrain="0,0,2,2"/>
+ <tile id="133" terrain="2,0,2,0"/>
+ <tile id="134" terrain="2,2,0,0"/>
+</tileset>
